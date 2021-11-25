2beafd29c90fb90cb6a167047e77c16c835def09
Cody Robibero <cody@robibe.ro>
2021-11-25 10:03:33 -0700

    reloate services

--- src/Jellyfin.Blazor/Program.cs
+++ src/Jellyfin.Blazor/Program.cs
@@ -2,7 +2,6 @@ using Blazor.Extensions.Logging;
 using Jellyfin.Blazor;
 using Jellyfin.Blazor.Services;
 using Jellyfin.Sdk;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -15,85 +14,12 @@ builder.Services.AddLogging(logger =>
     logger.AddBrowserConsole();
 });
 
-builder.Services.AddOptions();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<JellyfinAuthStateProvider, JellyfinAuthStateProvider>();
-builder.Services.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<JellyfinAuthStateProvider>());
-
 builder.Services.AddScoped(_ => new HttpClient
 {
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
-// Register services
-builder.Services.AddSingleton<IStateService, StateService>();
-builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
-builder.Services.AddScoped<ILibraryService, LibraryService>();
-
-// Register sdk services
-builder.Services.AddSingleton<SdkClientSettings>();
-
-// TODO remove unused services.
-builder.Services.AddHttpClient<IActivityLogClient, ActivityLogClient>();
-builder.Services.AddHttpClient<IApiKeyClient, ApiKeyClient>();
-builder.Services.AddHttpClient<IArtistsClient, ArtistsClient>();
-builder.Services.AddHttpClient<IAudioClient, AudioClient>();
-builder.Services.AddHttpClient<IBrandingClient, BrandingClient>();
-builder.Services.AddHttpClient<IChannelsClient, ChannelsClient>();
-builder.Services.AddHttpClient<ICollectionClient, CollectionClient>();
-builder.Services.AddHttpClient<IConfigurationClient, ConfigurationClient>();
-builder.Services.AddHttpClient<IDashboardClient, DashboardClient>();
-builder.Services.AddHttpClient<IDevicesClient, DevicesClient>();
-builder.Services.AddHttpClient<IDisplayPreferencesClient, DisplayPreferencesClient>();
-builder.Services.AddHttpClient<IDlnaClient, DlnaClient>();
-builder.Services.AddHttpClient<IDlnaServerClient, DlnaServerClient>();
-builder.Services.AddHttpClient<IDynamicHlsClient, DynamicHlsClient>();
-builder.Services.AddHttpClient<IEnvironmentClient, EnvironmentClient>();
-builder.Services.AddHttpClient<IFilterClient, FilterClient>();
-builder.Services.AddHttpClient<IGenresClient, GenresClient>();
-builder.Services.AddHttpClient<IHlsSegmentClient, HlsSegmentClient>();
-builder.Services.AddHttpClient<IImageClient, ImageClient>();
-builder.Services.AddHttpClient<IImageByNameClient, ImageByNameClient>();
-builder.Services.AddHttpClient<IInstantMixClient, InstantMixClient>();
-builder.Services.AddHttpClient<IItemLookupClient, ItemLookupClient>();
-builder.Services.AddHttpClient<IItemRefreshClient, ItemRefreshClient>();
-builder.Services.AddHttpClient<IItemsClient, ItemsClient>();
-builder.Services.AddHttpClient<ILibraryClient, LibraryClient>();
-builder.Services.AddHttpClient<IItemUpdateClient, ItemUpdateClient>();
-builder.Services.AddHttpClient<ILibraryStructureClient, LibraryStructureClient>();
-builder.Services.AddHttpClient<ILiveTvClient, LiveTvClient>();
-builder.Services.AddHttpClient<ILocalizationClient, LocalizationClient>();
-builder.Services.AddHttpClient<IMediaInfoClient, MediaInfoClient>();
-builder.Services.AddHttpClient<IMoviesClient, MoviesClient>();
-builder.Services.AddHttpClient<IMusicGenresClient, MusicGenresClient>();
-builder.Services.AddHttpClient<INotificationsClient, NotificationsClient>();
-builder.Services.AddHttpClient<IPackageClient, PackageClient>();
-builder.Services.AddHttpClient<IPersonsClient, PersonsClient>();
-builder.Services.AddHttpClient<IPlaylistsClient, PlaylistsClient>();
-builder.Services.AddHttpClient<IPlaystateClient, PlaystateClient>();
-builder.Services.AddHttpClient<IPluginsClient, PluginsClient>();
-builder.Services.AddHttpClient<IQuickConnectClient, QuickConnectClient>();
-builder.Services.AddHttpClient<IRemoteImageClient, RemoteImageClient>();
-builder.Services.AddHttpClient<IScheduledTasksClient, ScheduledTasksClient>();
-builder.Services.AddHttpClient<ISearchClient, SearchClient>();
-builder.Services.AddHttpClient<ISessionClient, SessionClient>();
-builder.Services.AddHttpClient<IStartupClient, StartupClient>();
-builder.Services.AddHttpClient<IStudiosClient, StudiosClient>();
-builder.Services.AddHttpClient<ISubtitleClient, SubtitleClient>();
-builder.Services.AddHttpClient<ISuggestionsClient, SuggestionsClient>();
-builder.Services.AddHttpClient<ISyncPlayClient, SyncPlayClient>();
-builder.Services.AddHttpClient<ISystemClient, SystemClient>();
-builder.Services.AddHttpClient<ITimeSyncClient, TimeSyncClient>();
-builder.Services.AddHttpClient<ITrailersClient, TrailersClient>();
-builder.Services.AddHttpClient<ITvShowsClient, TvShowsClient>();
-builder.Services.AddHttpClient<IUniversalAudioClient, UniversalAudioClient>();
-builder.Services.AddHttpClient<IUserClient, UserClient>();
-builder.Services.AddHttpClient<IUserLibraryClient, UserLibraryClient>();
-builder.Services.AddHttpClient<IUserViewsClient, UserViewsClient>();
-builder.Services.AddHttpClient<IVideoAttachmentsClient, VideoAttachmentsClient>();
-builder.Services.AddHttpClient<IVideoHlsClient, VideoHlsClient>();
-builder.Services.AddHttpClient<IVideosClient, VideosClient>();
-builder.Services.AddHttpClient<IYearsClient, YearsClient>();
+builder.Services.AddServices();
 
 var host = builder.Build();
 var clientSettings = host.Services.GetRequiredService<SdkClientSettings>();
---
+++ src/Jellyfin.Blazor/Services/Extensions.cs
@@ -0,0 +1,92 @@
+﻿using Jellyfin.Sdk;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Jellyfin.Blazor.Services;
+
+/// <summary>
+/// Service Extensions.
+/// </summary>
+public static class Extensions
+{
+    /// <summary>
+    /// Adds the required services.
+    /// </summary>
+    /// <param name="serviceCollection">Instance of the <see cref="IServiceCollection"/>.</param>
+    public static void AddServices(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddOptions();
+        serviceCollection.AddAuthorizationCore();
+        serviceCollection.AddScoped<JellyfinAuthStateProvider, JellyfinAuthStateProvider>();
+        serviceCollection.AddScoped<AuthenticationStateProvider>(p => p.GetRequiredService<JellyfinAuthStateProvider>());
+        
+        // Register services
+        serviceCollection.AddSingleton<IStateService, StateService>();
+        serviceCollection.AddScoped<IAuthenticationService, AuthenticationService>();
+        serviceCollection.AddScoped<ILibraryService, LibraryService>();
+
+        // Register sdk services
+        serviceCollection.AddSingleton<SdkClientSettings>();
+
+        // TODO remove unused serviceCollection.
+        serviceCollection.AddHttpClient<IActivityLogClient, ActivityLogClient>();
+        serviceCollection.AddHttpClient<IApiKeyClient, ApiKeyClient>();
+        serviceCollection.AddHttpClient<IArtistsClient, ArtistsClient>();
+        serviceCollection.AddHttpClient<IAudioClient, AudioClient>();
+        serviceCollection.AddHttpClient<IBrandingClient, BrandingClient>();
+        serviceCollection.AddHttpClient<IChannelsClient, ChannelsClient>();
+        serviceCollection.AddHttpClient<ICollectionClient, CollectionClient>();
+        serviceCollection.AddHttpClient<IConfigurationClient, ConfigurationClient>();
+        serviceCollection.AddHttpClient<IDashboardClient, DashboardClient>();
+        serviceCollection.AddHttpClient<IDevicesClient, DevicesClient>();
+        serviceCollection.AddHttpClient<IDisplayPreferencesClient, DisplayPreferencesClient>();
+        serviceCollection.AddHttpClient<IDlnaClient, DlnaClient>();
+        serviceCollection.AddHttpClient<IDlnaServerClient, DlnaServerClient>();
+        serviceCollection.AddHttpClient<IDynamicHlsClient, DynamicHlsClient>();
+        serviceCollection.AddHttpClient<IEnvironmentClient, EnvironmentClient>();
+        serviceCollection.AddHttpClient<IFilterClient, FilterClient>();
+        serviceCollection.AddHttpClient<IGenresClient, GenresClient>();
+        serviceCollection.AddHttpClient<IHlsSegmentClient, HlsSegmentClient>();
+        serviceCollection.AddHttpClient<IImageClient, ImageClient>();
+        serviceCollection.AddHttpClient<IImageByNameClient, ImageByNameClient>();
+        serviceCollection.AddHttpClient<IInstantMixClient, InstantMixClient>();
+        serviceCollection.AddHttpClient<IItemLookupClient, ItemLookupClient>();
+        serviceCollection.AddHttpClient<IItemRefreshClient, ItemRefreshClient>();
+        serviceCollection.AddHttpClient<IItemsClient, ItemsClient>();
+        serviceCollection.AddHttpClient<ILibraryClient, LibraryClient>();
+        serviceCollection.AddHttpClient<IItemUpdateClient, ItemUpdateClient>();
+        serviceCollection.AddHttpClient<ILibraryStructureClient, LibraryStructureClient>();
+        serviceCollection.AddHttpClient<ILiveTvClient, LiveTvClient>();
+        serviceCollection.AddHttpClient<ILocalizationClient, LocalizationClient>();
+        serviceCollection.AddHttpClient<IMediaInfoClient, MediaInfoClient>();
+        serviceCollection.AddHttpClient<IMoviesClient, MoviesClient>();
+        serviceCollection.AddHttpClient<IMusicGenresClient, MusicGenresClient>();
+        serviceCollection.AddHttpClient<INotificationsClient, NotificationsClient>();
+        serviceCollection.AddHttpClient<IPackageClient, PackageClient>();
+        serviceCollection.AddHttpClient<IPersonsClient, PersonsClient>();
+        serviceCollection.AddHttpClient<IPlaylistsClient, PlaylistsClient>();
+        serviceCollection.AddHttpClient<IPlaystateClient, PlaystateClient>();
+        serviceCollection.AddHttpClient<IPluginsClient, PluginsClient>();
+        serviceCollection.AddHttpClient<IQuickConnectClient, QuickConnectClient>();
+        serviceCollection.AddHttpClient<IRemoteImageClient, RemoteImageClient>();
+        serviceCollection.AddHttpClient<IScheduledTasksClient, ScheduledTasksClient>();
+        serviceCollection.AddHttpClient<ISearchClient, SearchClient>();
+        serviceCollection.AddHttpClient<ISessionClient, SessionClient>();
+        serviceCollection.AddHttpClient<IStartupClient, StartupClient>();
+        serviceCollection.AddHttpClient<IStudiosClient, StudiosClient>();
+        serviceCollection.AddHttpClient<ISubtitleClient, SubtitleClient>();
+        serviceCollection.AddHttpClient<ISuggestionsClient, SuggestionsClient>();
+        serviceCollection.AddHttpClient<ISyncPlayClient, SyncPlayClient>();
+        serviceCollection.AddHttpClient<ISystemClient, SystemClient>();
+        serviceCollection.AddHttpClient<ITimeSyncClient, TimeSyncClient>();
+        serviceCollection.AddHttpClient<ITrailersClient, TrailersClient>();
+        serviceCollection.AddHttpClient<ITvShowsClient, TvShowsClient>();
+        serviceCollection.AddHttpClient<IUniversalAudioClient, UniversalAudioClient>();
+        serviceCollection.AddHttpClient<IUserClient, UserClient>();
+        serviceCollection.AddHttpClient<IUserLibraryClient, UserLibraryClient>();
+        serviceCollection.AddHttpClient<IUserViewsClient, UserViewsClient>();
+        serviceCollection.AddHttpClient<IVideoAttachmentsClient, VideoAttachmentsClient>();
+        serviceCollection.AddHttpClient<IVideoHlsClient, VideoHlsClient>();
+        serviceCollection.AddHttpClient<IVideosClient, VideosClient>();
+        serviceCollection.AddHttpClient<IYearsClient, YearsClient>();
+    }
+}
